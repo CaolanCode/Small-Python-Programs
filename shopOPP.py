b384bbf579719d30835a638ba1399c2f7c3cb960
CaolanCode <cmacmahon3@gmail.com>
2022-11-27 12:38:14 +0000

    commit with child class

--- shopOPP.py
+++ shopOPP.py
@@ -23,7 +23,7 @@ class Item:
         self.price = self.price * self.pay_rate
 
     def __repr__(self):
-        return f"Item('{self.name}','{self.price}','{self.quantity}')"
+        return f"{self.__class__.__name__}('{self.name}','{self.price}','{self.quantity}')"
 
     @classmethod
     def instantiate_from_csv(cls):
@@ -48,7 +48,21 @@ class Item:
             return False
 
 
-print(Item.is_integer(7.0))
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity=0, broken_phone=0):  # default parameter
+        super().__init__(
+            name, price, quantity
+        )
+        # Run validation to the received arguments
+        assert broken_phone >= 0, f"Broken phones {broken_phone} is not greater than or equal to zero!"
+        # Assign to self object
+        self.broken_phones = broken_phone
 
 
-Item.instantiate_from_csv()
+phone1 = Phone("jscPhonev10", 500, 5, 1)
+print(phone1.calculate_total_price())
+phone2 = Phone("jscPhonev20", 700, 5)
+
+print(Item.all)
+print(Phone.all)
