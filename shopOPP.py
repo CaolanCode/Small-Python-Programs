f9833aaa571eaaf92e2ea4b84cb42651a90a7f96
CaolanCode <cmacmahon3@gmail.com>
2022-11-27 12:10:19 +0000

    shopOPP using csv

--- shopOPP.py
+++ shopOPP.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     pay_rate = 0.8  # pay rate after 20% discount
     all = []
@@ -22,11 +25,30 @@ class Item:
     def __repr__(self):
         return f"Item('{self.name}','{self.price}','{self.quantity}')"
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
 
-print(Item.all)
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):  # count out the floats that are .0
+            return num.is_integer()  # checks if float is .0
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+
+print(Item.is_integer(7.0))
+
+
+Item.instantiate_from_csv()
